5f06506ddd613dc654cce5dae4aee6ea016a0a15
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-02-12 22:20:51 +0000

    improve view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,26 +1,39 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   def new
     @list = List.new
-    @movie = Movie.where("name LIKE ?", "%#{params[:query]}%")
   end
 
   def create
     @list = List.new(list_params)
-    @list.save
-    redirect_to lists_path
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
   end
 
   private
 
+  def set_list
+    @list = List.find(params[:id])
+  end
+
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :movie, :list, presence: true
   validates :movie_id, uniqueness: { scope: :list_id, message: "has already been bookmarked in this list" }
   validates :comment, length: { minimum: 6 }
 end
