b6248454cf62f6f3a3fe9f6e3725a26c04f34f5c
Vincent Chaussepied <vincent.chaussepied@gmail.com>
2024-02-15 20:27:22 +0000

    add movies to lists

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :lists, only: %i[index show new create destroy] do
     resources :bookmarks, only: %i[new create destroy]
   end
+  resources :bookmarks, only: :destroy
 end
